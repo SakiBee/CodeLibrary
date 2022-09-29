ca2095ff53608b163d27ffbad15379fc555496b9
Md. Sakib Uddin <63546044+SakiBee@users.noreply.github.com>
2022-09-29 11:45:31 +0600

    Update and rename LCS Recursive to LCS Recursive.cpp

--- DP/LCS Recursive.cpp
+++ DP/LCS Recursive.cpp
@@ -5,11 +5,10 @@ int dp[N][N];
 
 //Recursive
 int LCS(string a, string b, int i, int j) {
-	if(i < 0 || j < 0) return 0;
+  if(i < 0 || j < 0) return 0;
   if(dp[i][j] != -1) return dp[i][j];
-  
-	if(a[i] == b[j]) dp[i][j] = 1 + LCS(a, b, i - 1, j - 1);
-	else dp[i][j] = max(LCS(a, b, i - 1, j), LCS(a, b, i, j - 1));
+  if(a[i] == b[j]) dp[i][j] = 1 + LCS(a, b, i - 1, j - 1);
+  else dp[i][j] = max(LCS(a, b, i - 1, j), LCS(a, b, i, j - 1));
   
   return dp[i][j];
 }
