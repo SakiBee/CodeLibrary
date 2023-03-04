3588363752420d62ac8c963f279385a03e2d4932
Md. Sakib Uddin <63546044+SakiBee@users.noreply.github.com>
2023-03-04 09:43:50 +0600

    Update (Kahn's algo) Topological Sorting.cpp

--- Sorting Algorithms/(Kahn's algo) Topological Sorting.cpp
+++ Sorting Algorithms/(Kahn's algo) Topological Sorting.cpp
@@ -1,47 +1,48 @@
-#include<bits/stdc++.h>
+//https://cses.fi/problemset/task/1679
+//https://leetcode.com/problems/course-schedule-ii/
+
+#include <bits/stdc++.h>
 using namespace std;
-#define pb push_back
-#define     optimize        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+const int N = 1e6+7;
 
-const int M = 1000001;
-std::vector<int> adj[M],res;
-bool vis[M];
-int inDeg[M];
+vector<int> g[N], indeg(N);
+bool vis[N];
 
-bool kahns_algo (int n)
-{
-	//queue<int>q;
-	priority_queue<int,vector<int>,greater<int> >q;
-	for(int i=1; i<=n; i++) if(inDeg[i] == 0) q.push(i);
-
-	while(!q.empty())
-	{
-		int temp = q.top();
-		q.pop();
-		res.pb(temp);
-
-		for(auto child: adj[temp]) 
-		{
-			inDeg[child]--;
-			if(inDeg[child] == 0) q.push(child);
-		}
-	}
-	return res.size() == n;
-}
-
-
-int main()
-{
-	optimize;
-	int n,m,a,b,t;
-	cin >> n >> m;
-	for(int i=1; i<=m; i++) cin >> a >> b, adj[a].pb(b), inDeg[b]++;
-
-	if(!kahns_algo(n)) cout << "Sandro fails.\n";
-	//cout << "After Topological Sort\n";
-	else{
-		for(auto ans : res) cout << ans << " ";
-		cout << '\n';}
-
-
+int32_t main () {
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int n, m; cin >> n >> m;
+  int x, y;
+  for (int i = 0; i < m; i++) {
+  	cin >> x >> y;
+  	g[x].push_back(y);
+    indeg[y]++;
+  }
+  //ready can be a queue
+  vector<int> ready, ans;
+  for (int i = 1; i <= n; i++) {
+    if(indeg[i] == 0) {
+      ready.push_back(i);
+      vis[i] = 1;
+    }
+  }
+  while(ans.size() < n) {
+    if(ready.empty()) {
+      cout << "IMPOSSIBLE\n";
+      return 0;
+    }
+    int cur = ready.back();
+    ready.pop_back();
+    ans.push_back(cur);
+    for (auto u : g[cur]) {
+      indeg[u]--;
+      if(!vis[u] and indeg[u] == 0) {
+        ready.push_back(u);
+        vis[u] = 1;
+      }
+    }
+  }
+  
+  for (auto u : ans) cout << u << " ";
+  cout << endl;
+  
 }
